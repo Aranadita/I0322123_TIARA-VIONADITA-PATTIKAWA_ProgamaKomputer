3a4419e03b7b7b0fbcddcc490ea011b80cda5a0c
TiaraPattikawa <Tiarapattikawa07@student.uns.ac.id>
2023-03-06 11:42:34 +0700

    Commit pertama

---
+++ I0322123_TIARA-VIONADITA-PATTIKAWA_ProgamaKomputer/Modul_01/TiaraPattikawa.py
@@ -0,0 +1,12 @@
+Nama_depan    = "Tiara"
+Nama_belakang = "Pattikawa"
+Kelas         = "D"
+
+angka = 10
+desimal = 0,5
+boolean = True
+Kelas_D = {"Tiara", "kelas D", "dll"}
+
+print(Nama_depan, Nama_belakang)
+print("Namaku Tiara panggil Ara aja")
+print("Aku dari kelas", Kelas)
